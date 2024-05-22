6f578509fb84c26a11de2983a333474877f29501
ikun <0215.miyatto@gmail.com>
2024-05-23 05:36:35 +0900

    supabaseの設定完了

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,9 +1,18 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
 	const supabase = createServerComponentClient({ cookies });
+	const { data: lessons, error } = await supabase.from('lesson').select('*');
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">Hello</main>
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			{lessons?.map((lesson: any) => (
+				<Link href={`/${lesson.id}`} key={lesson.id}>
+					{lesson.title}
+				</Link>
+			))}
+		</main>
 	);
 }
