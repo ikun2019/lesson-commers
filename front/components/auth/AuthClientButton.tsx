63ccd21e75d598490bc471c8bf22ec6951d326ff
ikun <0215.miyatto@gmail.com>
2024-05-24 18:54:33 +0900

    認証機能の実装完了

--- front/components/auth/AuthClientButton.tsx
+++ front/components/auth/AuthClientButton.tsx
@@ -3,19 +3,39 @@
 import React from 'react';
 import { Session } from '@supabase/auth-helpers-nextjs';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
 
 import { Button } from '../ui/button';
 
 const AuthClientButton = ({ session }: { session: Session | null }) => {
+	const router = useRouter();
 	const supabase = createClientComponentClient();
 
+	// * Loginメソッド
 	const handleLogin = async () => {
 		await supabase.auth.signInWithOAuth({
 			provider: 'github',
+			options: {
+				redirectTo: `${location.origin}/auth/callback`,
+			},
 		});
 	};
 
-	return <Button onClick={handleLogin}>Login</Button>;
+	// * Logoutメソッド
+	const handleLogout = async () => {
+		await supabase.auth.signOut();
+		router.refresh();
+	};
+
+	return (
+		<>
+			{session ? (
+				<Button onClick={handleLogout}>Logout</Button>
+			) : (
+				<Button onClick={handleLogin}>Login</Button>
+			)}
+		</>
+	);
 };
 
 export default AuthClientButton;
