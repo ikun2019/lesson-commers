8f0709ea77cec836130d0d52e25a529ee73acb70
ikun <0215.miyatto@gmail.com>
2024-05-27 05:54:13 +0900

    ビデオコンテンツの埋め込み

--- front/app/[id]/page.tsx
+++ front/app/[id]/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { cookies } from 'next/headers';
+import { YouTubeEmbed } from '@next/third-parties/google';
 import { createServerComponentClient, SupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { Database } from '@/lib/database.types';
+import { extractYouTubeVideoId } from '@/utils/extractYoutubeVideoId';
 
 // * Single Lessonを取得するメソッド
 const getDetailLesson = async (id: number, supabase: SupabaseClient<Database>) => {
@@ -26,13 +28,13 @@ const LessonDetailPage = async ({ params }: { params: { id: number } }) => {
 	const supabase = createServerComponentClient<Database>({ cookies });
 	const lesson = await getDetailLesson(params.id, supabase);
 	const video = await getPremiumContent(params.id, supabase);
-	console.log('LessonDetailPage =>', lesson);
-	console.log('video =>', video);
+	const videoId = extractYouTubeVideoId(video?.video_url) as string;
 
 	return (
 		<div className="w-full max-3-3xl mx-auto py-16 px-8">
 			<h1 className="text-3xl mb-6">{lesson?.title}</h1>
 			<p className="mb-8">{lesson?.description}</p>
+			<YouTubeEmbed height={400} videoid={videoId} />
 		</div>
 	);
 };
