a97a54a49358144f193200e5c6bfbeef6d2ada8b
ikun <0215.miyatto@gmail.com>
2024-06-01 11:50:54 +0900

    ヘッダーにdashboardボタンを追加

--- front/components/Header.tsx
+++ front/components/Header.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+
 import { Button } from './ui/button';
 import Link from 'next/link';
 import AuthServerButton from './auth/AuthServerButton';
 
-const Header = () => {
+const Header = async () => {
+	const supabase = createServerComponentClient({ cookies });
+	const { data: user } = await supabase.auth.getSession();
+	console.log('user =>', user);
+
 	return (
 		<div className="flex py-4 px-6 border-gray-200">
 			<Link href={'/'}>
 				<Button variant="outline">Home</Button>
 			</Link>
+			{user.session && (
+				<Link href="/dashboard" className="ml-4">
+					<Button variant="outline">ダッシュボード</Button>
+				</Link>
+			)}
 			<Link href={'/pricing'} className="ml-4">
 				<Button variant="outline">Price</Button>
 			</Link>
