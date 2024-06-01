847f5b8b374279fd163c205cbd25811567802605
ikun <0215.miyatto@gmail.com>
2024-06-01 17:45:24 +0900

    完成

--- front/app/api/stripe-hooks/route.ts
+++ front/app/api/stripe-hooks/route.ts
@@ -22,19 +22,41 @@ export async function POST(req: NextRequest) {
 				await supabase
 					.from('profile')
 					.update({
-						is_subscribe: true,
+						is_subscribed: true,
 						interval: customerSubscriptionCreated.items.data[0].plan.interval,
 					})
 					.eq('stripe_customer', event.data.object.customer);
 				break;
 			case 'customer.subscription.deleted':
-				const customerSubscriptionDeleted = event.data.object;
-				// Then define and call a function to handle the event customer.subscription.deleted
+				await supabase
+					.from('profile')
+					.update({
+						is_subscribed: false,
+						interval: null,
+					})
+					.eq('stripe_customer', event.data.object.customer);
 				break;
 			case 'customer.subscription.updated':
 				const customerSubscriptionUpdated = event.data.object;
-				// Then define and call a function to handle the event customer.subscription.updated
-				break;
+				if (customerSubscriptionUpdated.status === 'canceled') {
+					await supabase
+						.from('profile')
+						.update({
+							is_subscribed: false,
+							interval: null,
+						})
+						.eq('stripe_customer', event.data.object.customer);
+					break;
+				} else {
+					await supabase
+						.from('profile')
+						.update({
+							is_subscribed: true,
+							interval: customerSubscriptionUpdated.items.data[0].plan.interval,
+						})
+						.eq('stripe_customer', event.data.object.customer);
+					break;
+				}
 			// ... handle other event types
 			default:
 				console.log(`Unhandled event type ${event.type}`);
@@ -43,5 +65,4 @@ export async function POST(req: NextRequest) {
 	} catch (err: any) {
 		return NextResponse.json(`Webhook Error: ${err.message}`, { status: 401 });
 	}
-	console.log(event);
 }
